bab67f926e62c5a77bba8b4f91c66a8b6a60da31
Francisco Scovino <franciscoscovino@gmail.com>
2022-01-16 11:40:37 -0500

    Adds crossorigin annotation to controller

--- src/main/java/com/scovino/kanban/controller/TaskController.java
+++ src/main/java/com/scovino/kanban/controller/TaskController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.scovino.kanban.model.Task;
 import com.scovino.kanban.service.TaskService;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/tasks/")
 public class TaskController {
 
