6a79dcd6099e719aaa54316ae558ba5e2519670d
Francisco Scovino <franciscoscovino@gmail.com>
2022-01-17 17:59:14 -0500

    adds cors configurer

--- src/main/java/com/scovino/kanban/KanbanApplication.java
+++ src/main/java/com/scovino/kanban/KanbanApplication.java
@@ -2,12 +2,25 @@ package com.scovino.kanban;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class KanbanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KanbanApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/tasks/").allowedOrigins("http://localhost:3000/");
+			}
+		};
 	}
 
 }
